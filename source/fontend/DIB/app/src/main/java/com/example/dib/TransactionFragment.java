5d7de1cf4c087622717253744045b31cafcf5eac
Bùi Thị Phương Anh <522000004@student.tdt.edu.vn>
2025-05-10 18:50:58 +0700

    UI

--- source/fontend/DIB/app/src/main/java/com/example/dib/TransactionFragment.java
+++ source/fontend/DIB/app/src/main/java/com/example/dib/TransactionFragment.java
@@ -1,5 +1,6 @@
 package com.example.dib;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dib.payment.BillElectric;
+import com.example.dib.payment.BillWater;
+import com.example.dib.payment.BookPlane;
+import com.example.dib.payment.Transfer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,23 +29,39 @@ public class TransactionFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.transaction_fragment, container, false);
 
-        // Tìm RecyclerView trong layout
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Tạo danh sách dữ liệu
         List<TransactionItem> items = new ArrayList<>();
         items.add(new TransactionItem(R.drawable.ic_transfer, "Chuyển tiền"));
-        items.add(new TransactionItem(R.drawable.ic_qr, "Quét mã QR"));
-        items.add(new TransactionItem(R.drawable.ic_recharge, "Nạp tiền"));
-        items.add(new TransactionItem(R.drawable.ic_payment, "Thanh toán hóa đơn"));
+        items.add(new TransactionItem(R.drawable.ic_payment, "Thanh toán hóa đơn điện"));
+        items.add(new TransactionItem(R.drawable.ic_payment, "Thanh toán hóa đơn nước"));
         items.add(new TransactionItem(R.drawable.ic_book_airplane, "Đặt vé máy bay"));
-        items.add(new TransactionItem(R.drawable.ic_book_car, "Đặt vé xe khách"));
-        items.add(new TransactionItem(R.drawable.ic_transfer_inter, "Chuyển tiền quốc tế"));
         items.add(new TransactionItem(R.drawable.ic_recharge_card, "Nạp tiền điện thoại"));
 
-        // Gán adapter cho RecyclerView
-        TransactionAdapter adapter = new TransactionAdapter(items);
+        TransactionAdapter adapter = new TransactionAdapter(items, item -> {
+            String title = item.getTitle();
+            switch (title) {
+                case "Chuyển tiền":
+                    startActivity(new Intent(getActivity(), Transfer.class));
+                    break;
+                case "Thanh toán hóa đơn nước":
+                    startActivity(new Intent(getActivity(), BillWater.class));
+                    break;
+                case "Thanh toán hóa đơn điện":
+                    startActivity(new Intent(getActivity(), BillElectric.class));
+                    break;
+                case "Đặt vé máy bay":
+                    startActivity(new Intent(getActivity(), BookPlane.class));
+                    break;
+//                case "Nạp tiền điện thoại":
+//                    startActivity(new Intent(getActivity(), RechargePhoneActivity.class));
+//                    break;
+                default:
+                    break;
+            }
+        });
+
         recyclerView.setAdapter(adapter);
 
         return view;
